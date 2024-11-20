57d603539c1d4a5cb0aae722c902cb46c2fc2683
Mohammad Nafisi <nafisidev@gmail.com>
2024-11-19 22:52:19 -0800

    First version

---
+++ src/app/questions/page.tsx
@@ -0,0 +1,23 @@
+export default function Principles() {
+    return (
+      <div className="h-screen w-screen bg-black flex items-center justify-center">
+        <main>
+          <div className="h-[calc(10vh-2rem)] w-[calc(99vw-2rem)] p-8 border border-white box-border flex items-center">
+            <h1 className="text-2xl font-bold text-white">MOHAMMAD NAFISI</h1>
+          </div>
+          <div className="h-[calc(85vh-2rem)] w-[calc(99vw-2rem)] p-8 border border-white box-border flex items-center">
+            <div className="flex w-full items-center">
+              <p>Here are the list of principles that have shaped my worldview in a disporportionate way.</p>
+              <p>The power law and normal distribution</p>
+              <p>Confirmation Bias</p>
+              <p>Sexual Dimorphism</p>
+              <p>Evolution</p>
+              <p>Inversion</p>
+              <p>Framing</p>
+              
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
